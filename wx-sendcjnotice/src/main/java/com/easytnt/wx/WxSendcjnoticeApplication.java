ebefeeed17d36639c7ed2d684cefa3b37333e586
liuyu <306487103@qq.com>
2018-08-08 14:44:49 +0800

    增加微信发送成绩的后台操作

---
+++ wx-sendcjnotice/src/main/java/com/easytnt/wx/WxSendcjnoticeApplication.java
@@ -0,0 +1,12 @@
+package com.easytnt.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxSendcjnoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxSendcjnoticeApplication.class, args);
+    }
+}
