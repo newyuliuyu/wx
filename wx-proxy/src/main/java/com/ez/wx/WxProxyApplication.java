468a6bf61f478f5f01cbab5202b4f5826c6cf238
liuyu <306487103@qq.com>
2018-08-20 18:30:05 +0800

    wxproxy项目web启动

--- wx-proxy/src/main/java/com/ez/wx/WxProxyApplication.java
+++ wx-proxy/src/main/java/com/ez/wx/WxProxyApplication.java
@@ -4,9 +4,18 @@ import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class})
-public class WxProxyApplication {
+public class WxProxyApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        application.sources(WxProxyApplication.class);
+        return super.configure(application);
+    }
+
 
     public static void main(String[] args) {
 
